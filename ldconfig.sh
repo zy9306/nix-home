c6dc6e685b4600a78c4586578c152e0cadc1c668
Chtholly <zy99306@outlook.com>
2021-01-27 21:07:05 +0800

    enhance ldconfig

--- ldconfig.sh
+++ ldconfig.sh
@@ -4,16 +4,32 @@
 
 LD_LIBRARY_PATH_VAR=$LD_LIBRARY_PATH
 
-for dir in $(find /nix/store -type d -wholename "*gcc*lib*/lib")
-do
-    if [[ $LD_LIBRARY_PATH_VAR != *"$dir"* ]];then
-        if [ ! $LD_LIBRARY_PATH_VAR ];then
-            LD_LIBRARY_PATH_VAR="$dir"
-        else
-            LD_LIBRARY_PATH_VAR="$LD_LIBRARY_PATH_VAR:$dir"
+# 方案一：直接从 /nix/store 中找文件，加到 LD_LIBRARY_PATH 中，会将所有版本都添加
+find_version(){
+    for dir in $(find /nix/store -type d -wholename "*gcc*lib*/lib")
+    do
+        if [[ $LD_LIBRARY_PATH_VAR != *"$dir"* ]];then
+            if [ ! $LD_LIBRARY_PATH_VAR ];then
+                LD_LIBRARY_PATH_VAR="$dir"
+            else
+                LD_LIBRARY_PATH_VAR="$LD_LIBRARY_PATH_VAR:$dir"
+            fi
         fi
-    fi
-done
+    done
+}
+
+# 方案二：利用 nix 找出相关路径，速度较快，只添加当前版本
+nix_version(){
+    LD_LIBRARY_PATH_VAR=$(nix eval --raw nixpkgs.stdenv.cc.cc.lib)/lib
+}
+
+# 方案三：当文件依赖于特定版本的动态库时，使用 patchelf 对其进行 patch
+# https://github.com/NixOS/patchelf
+# Display shared library dependencies of a binary
+# ldd path/to/binary
+
+
+nix_version
 
 if [ $LD_LIBRARY_PATH_VAR ];then
     LD_LIBRARY_PATH_VAR="$LD_LIBRARY_PATH_VAR:$HOME/.nix-profile/lib"
@@ -21,6 +37,6 @@ else
     LD_LIBRARY_PATH_VAR="$HOME/.nix-profile/lib"
 fi
 
-echo -e "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH_VAR"
+echo -e "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH_VAR" > $HOME/.ldconfig
 
 unset LD_LIBRARY_PATH_VAR
