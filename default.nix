d67fef355589e16d607c6a7ba3593a4e63755aa7
Arcueid <zy99306@outlook.com>
2021-03-01 18:20:30 +0800

    NOTE: clear file; build

--- default.nix
+++ default.nix
@@ -1,11 +1,9 @@
 let
   sources = import ./nix/sources.nix;
 
-  nixpkgs = import sources."nixpkgs-release-20.09" { };
-
-  nixpkgs-unstable = import sources."nixpkgs-unstable" { };
+  nixpkgs = import sources."nixpkgs-unstable" { };
 
   pkgs = import <nixpkgs> { };
 
   extra = import ./nix-extra;
-in { inherit nixpkgs nixpkgs-unstable pkgs extra; }
+in { inherit nixpkgs pkgs extra; }
--- nix-emacs/default.nix
+++ nix-emacs/default.nix
@@ -1,9 +1,7 @@
 let
   sources = import ../nix/sources.nix;
-  nixpkgs = sources."nixpkgs-release-20.09";
+  nixpkgs = sources."nixpkgs-unstable";
   emacs-overlay = sources."emacs-overlay";
-  emacs-overlay-001083a = sources."emacs-overlay-001083a";
-  emacs-overlay-7678979 = sources."emacs-overlay-7678979";
   pkgs = import nixpkgs {
     overlays = [
     (import (builtins.fetchTarball {
@@ -11,44 +9,12 @@ let
     }))
     ];
   };
-  pkgs-emacs-001083a = import nixpkgs {
-    overlays = [
-    (import (builtins.fetchTarball {
-      url = emacs-overlay-001083a.url;
-    }))
-    ];
-  };
-  pkgs-emacs-7678979 = import nixpkgs {
-    overlays = [
-    (import (builtins.fetchTarball {
-      url = emacs-overlay-7678979.url;
-    }))
-    ];
-  };
 in
 {
   # rename ctags to ctags.emacs
-  # 28.0.50
-  # emacsGcc
   emacsGcc = pkgs.emacsGcc.overrideAttrs (old: {
     configureFlags = old.configureFlags ++ ["--program-transform-name='s/^ctags$/ctags.emacs/'"];
   });
-  emacsGcc20201217 = pkgs-emacs-001083a.emacsGcc.overrideAttrs (old: {
-    configureFlags = old.configureFlags ++ ["--program-transform-name='s/^ctags$/ctags.emacs/'"];
-  });
-  emacsGcc20201017 = pkgs-emacs-7678979.emacsGcc.overrideAttrs (old: {
-    configureFlags = old.configureFlags ++ ["--program-transform-name='s/^ctags$/ctags.emacs/'"];
-  });
-
-  # emacsPgtkGcc
-  emacsPgtkGcc20201213 = pkgs-emacs-001083a.emacsPgtkGcc.overrideAttrs (old: {
-    configureFlags = old.configureFlags ++ ["--program-transform-name='s/^ctags$/ctags.emacs/'"];
-  });
-
-  emacsPgtk20201122 = pkgs-emacs-001083a.emacsPgtk.overrideAttrs (old: {
-    configureFlags = old.configureFlags ++ ["--program-transform-name='s/^ctags$/ctags.emacs/'"];
-  });
-
   # emacsGit
   emacsGit = pkgs.emacsGit.overrideAttrs (old: {
     configureFlags = old.configureFlags ++ ["--program-transform-name='s/^ctags$/ctags.emacs/'"];
