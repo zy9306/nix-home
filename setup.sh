69b529611680e449d4859bcddf48abcbfba42ff4
Arcueid <zy99306@outlook.com>
2021-06-01 11:30:42 +0800

    adjust

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-style_normal='\033[0m'
-style_red='\033[31m'
+STYLE_NORMAL='\033[0m'
+STYLE_RED='\033[31m'
 
 echo_red() {
-    echo -e "${style_red}$@${style_normal}"
+    echo -e "${STYLE_RED}$@${STYLE_NORMAL}"
 }
 
 install_nix() {
@@ -14,27 +14,31 @@ install_nix() {
     curl -L https://nixos.org/nix/install | sh
 }
 
+update_nix_channel() {
+    echo_red ">>> update nix channel ..."
+    nix-channel --add https://mirrors.tuna.tsinghua.edu.cn/nix-channels/nixpkgs-unstable nixpkgs
+}
+
 install_cachix() {
     echo_red ">>> install cachix ..."
     nix-env -iA cachix -f https://cachix.org/api/v1/install
+    cachix use arcueid
 }
-
-# install_home_manager() {
-#     echo_red ">>> install home-manager ..."
-#     # 注意，不要删除该 channel，不然 home-manager 会无法工作
-#     nix-channel --add https://github.com/nix-community/home-manager/archive/release-20.09.tar.gz home-manager
-#     nix-channel --update
-#     nix-shell '<home-manager>' -A install
-# }
 
 set_all() {
     install_nix
+    update_nix_channel
     install_cachix
-    # install_home_manager
 }
+
+# cachix mirror
+# ~/.config/nix/nix.conf
+# make tsinghua mirror at the first.
+# substituters = https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store https://cache.nixos.org/
 
 HELP="support command: \n \
     install_nix \n \
+    update_nix_channel \n \
     install_cachix \n \
     \n \
     set_all for exec above all"
@@ -42,6 +46,9 @@ HELP="support command: \n \
 case "$1" in
     "install_nix")
         install_nix
+        ;;
+    "update_nix_channel")
+        update_nix_channel
         ;;
     "install_cachix")
         install_cachix
--- test/default.nix
+++
@@ -1,7 +0,0 @@
-let
-  pkgs = import <nixpkgs> { };
-
-  python38 = pkgs.python38Full;
-  python37 = pkgs.python37Full;
-  python36 = pkgs.python36Full;
-in { inherit python36 python37 python38; }
