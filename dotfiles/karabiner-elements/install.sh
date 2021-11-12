2af9a651eee3caa9462b9331cfa83cddff0c48e6
Arcueid <zy99306@outlook.com>
2021-11-12 18:06:27 +0800

    backup karabiner.json

--- dotfiles/karabiner-elements/install.sh
+++ dotfiles/karabiner-elements/install.sh
@@ -7,4 +7,7 @@ mkdir -p $KARABINER_CONFIG_DIR
 
 rm -f $KARABINER_CONFIG_DIR/*
 
+rm -f $KARABINER_DIR/karabiner.json
+
 ln -s $PWD/command-fb.json $KARABINER_CONFIG_DIR/command-fb.json
+ln -s $PWD/karabiner.json $KARABINER_DIR/karabiner.json
